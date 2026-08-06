ceb9ba4c6fdcb7740f3f70b3436eaf73597d0026
ankita20611 <paudelankita38@gmail.com>
2026-08-06 10:45:20 +0545

    add login and logout functionally

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from accounts.forms import SignupForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+
 # Create your views here.
-
 def signup_view(request):
-
-    if request.method == "POST":
+    if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            messages.success(request, "Account created successfully.")
-            return redirect('admin:index')  # Redirect to the admin index page after successful signup
+            messages.success(request, 'Account created successfully.')
+            return redirect('admin:index')  
     else:
-        form = SignupForm()
+        form=SignupForm()
     return render(request, 'accounts/signup.html', {'form': form})
-    
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, 'Logged in successfully.')
+            return redirect('admin:index')  
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        messages.success(request, 'Logged out successfully.')
+    return redirect('accounts:login')
